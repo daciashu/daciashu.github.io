46fd89c53993ec30da9a6e5d8af542efd1c53abf
daciashu <cal98017@byui.edu>
2019-09-21 17:11:29 -0400

    date

--- js/currentDate.js
+++ js/currentDate.js
@@ -1,2 +1,30 @@
-const options = {weekday:'long', day:'numeric', month:'long', year:'numeric'};
-document.getElementById('currentDate').textcontent = new Date().toLocaleDateString('en-US', options);
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+let d = newDate();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + " , " + monthName + " " + d.getDate() +", " + year;
+
+document.getElementById('currentDate').textcontent = fulldate;
