cab84faa207ddafd70b398cd44b8f26ab53deb61
daciashu <cal98017@byui.edu>
2019-10-10 21:14:06 -0400

    js

---
+++ lesson4/js/newDate.js
@@ -0,0 +1,30 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+let d = newDate();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
+let fulldate = dayName + " , " + monthName + " " + d.getDate() +", " + year;
+
+document.getElementById('currentDate').textcontent = fulldate;
--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,13 +1,3 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
-
-try {
-    var theDate = new Date;
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-    };
-document.getElementsById('theDate').innerHTML = new Date().toLocaleDateString("en-US", options);
