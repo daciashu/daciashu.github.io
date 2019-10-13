87c67f67302f197423b464878d0623651986e3a7
daciashu <cal98017@byui.edu>
2019-10-12 20:05:42 -0400

    js

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,6 +1,3 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
-
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-GB', options);
