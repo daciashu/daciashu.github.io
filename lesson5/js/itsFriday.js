61f08ce0fa662c7e04a86a73982876f7df14f37c
daciashu <cal98017@byui.edu>
2019-10-19 16:08:10 -0400

    testing

--- lesson5/js/itsFriday.js
+++ lesson5/js/itsFriday.js
@@ -1,7 +1,7 @@
 function itsFriday() {
     var n = new Date();
     var d = n.getDay();
-    if (d == 5) {
+    if (d == 6) {
         document.getElementById("itsFriday").style.display = "block";
     }
     else {
