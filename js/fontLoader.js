9d4c6ee9eccf6eb747bf3ad74bd70715ec071be6
daciashu <cal98017@byui.edu>
2019-10-24 16:25:03 -0400

    webFont correction

--- js/fontLoader.js
+++ js/fontLoader.js
@@ -1,4 +1,4 @@
-webkitConvertPointFromNodeToPage.load({
+webFont.load({
     google: {
         families: ['Anton', 'Raleway']
     }
--- lesson6/js/fontLoader.js
+++ lesson6/js/fontLoader.js
@@ -1,4 +1,4 @@
-webkitConvertPointFromNodeToPage.load({
+webFont.load({
     google: {
         families: ['Berkshire Swash','Poppins']
     }
