454316062e23a95f07d8e81984a2ac884778963a
daciashu <cal98017@byui.edu>
2019-10-25 10:45:43 -0400

    chilly

---
+++ lesson6/js/windChill.js
@@ -0,0 +1,19 @@
+function chillyWind() {
+    var t = parseFloat(document.getElementById('tempF').textContent);
+    var s = parseFloat(document.getElementById('speed').textContent);
+    var output = windChill(t, s);
+
+    document.getElementById("output").innerHTML = output + "°F";
+}
+
+function windChill(tempF, speed) {
+    var s = Math.pow(speed, 0.16);
+    var f = (35.74 + (0.6215 * tempF) - (35.75 * s) + (0.4275 * (tempF * s))).toFixed(1);
+
+    if ( t <= 50 && s > 3 ) {
+        return f;
+    }
+        else{
+            return "N/A";
+        }
+}
