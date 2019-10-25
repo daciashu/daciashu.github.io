e03390eb4150cbfc61459e0700b0bf48bf61b5fe
daciashu <cal98017@byui.edu>
2019-10-25 14:48:21 -0400

    hmm

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -3,14 +3,14 @@ function chillyWind() {
     var s = parseFloat(document.getElementById('speed').textContent);
     var output = windChill(t, s);
 
-    document.getElementById("output").textContent = output + "°F";
+    document.getElementById("output").textContent = output + &#8457;
 }
 
 function windChill(tempF, speed) {
     var s = Math.pow(speed, 0.16);
     var f = (35.74 + (0.6215 * tempF) - (35.75 * s) + (0.4275 * (tempF * s))).toFixed(1);
 
-    if( t <= 50 && s > 3 ) {
+    if( tempF <= 50 && speed > 3 ) {
         return f;
     }
         else{
