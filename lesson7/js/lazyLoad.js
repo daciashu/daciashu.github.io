562ff34d01a48e1dd2dd6b2a235b1224bc85afa1
daciashu <cal98017@byui.edu>
2019-10-31 16:46:39 -0400

    java

--- lesson7/js/lazyLoad.js
+++ lesson7/js/lazyLoad.js
@@ -1,30 +1,29 @@
-const images = document.querySelectorAll("[data-src]");
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
-
-    img.src = src;
-}
 
 const imgOptions = {
     threshold: 0,
     rootMargin: "0px 0px 50px 0px"
 };
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
-    })
-}, imgOptions);
+const loadImages = (images) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAtttribute('data-src');};
+    };
+}
 
-images.forEach(images => {
-    imgObserver.observe(image);
-});
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+        });
+    },   imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
