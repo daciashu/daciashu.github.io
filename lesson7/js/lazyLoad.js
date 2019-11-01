63ce0825f466a7e3aaf0f5b584c966c97f6aa4ee
daciashu <cal98017@byui.edu>
2019-11-01 08:54:23 -0400

    ll

--- lesson7/js/lazyLoad.js
+++ lesson7/js/lazyLoad.js
@@ -49,17 +49,15 @@ const loadImages = (image) => {
 if('IntersectionObserver' in window) {
     const imgObserver = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
-            if (!item.isIntersecting) {
-                return;
-            } else {
-                preloadImage(item.target);
-                imgObserver.unobserve(item.target);
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
             }
         });
     }, imgOptions);
 
     imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
+        observer.observe(img);
     });
 } else {
     imagesToLoad.forEach((img) => {
