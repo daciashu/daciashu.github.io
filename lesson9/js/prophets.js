350b4b592cdea26232dc25e247f281a17b5cc018
daciashu <cal98017@byui.edu>
2019-11-13 13:13:26 -0500

    prophets

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,10 +10,21 @@ fetch(requestURL)
             for (let i = 0; i < prophets.length; i++) {
                 let card = document.createElement('section');
                 let h2 = document.createElement('h2');
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let pic = document.createElement('img');
 
                 h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+                p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+                p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+                pic.setAttribute('src', prophets[i].imageurl);
+                pic.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+
 
                 card.appendChild(h2);
+                card.appendChild(p1);
+                card.appendChild(p2);
+                card.appendChild(pic);
 
                 document.querySelector('div.cards').appendChild(card);
             }
