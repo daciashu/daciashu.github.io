34fc65470759897782db69e7711b5affb5659173
daciashu <cal98017@byui.edu>
2019-11-15 20:42:21 -0500

    java sections etc

--- lesson9/js/towns.js
+++ lesson9/js/towns.js
@@ -11,12 +11,16 @@ fetch(requestURL)
             for (let i = 0; i < towns.length; i++) {
                 if (weatherPages.includes(towns[i].name)) {
                 let card = document.createElement('section');
+                let cardData = document.createElement('section')
                 let h2 = document.createElement('h2');
-                let p1 = document.createElement('em');
+                let p1 = document.createElement('p');
                 let p2 = document.createElement('p');
                 let p3 = document.createElement('p');
                 let p4 = document.createElement('p');
                 let pic = document.createElement('img');
+
+                cardData.className = 'town-data';
+                p1.className = 'motto';
 
                 h2.textContent = towns[i].name;
                 p1.textContent = towns[i].motto;
@@ -26,13 +30,14 @@ fetch(requestURL)
                 pic.setAttribute('src', "images/" + towns[i].photo);
                 pic.setAttribute('alt', "A picture of " + towns[i].name);
 
-
-                card.appendChild(h2);
-                card.appendChild(p1);
-                card.appendChild(p2);
-                card.appendChild(p3);
-                card.appendChild(p4);
+                card.appendChild(cardData);
                 card.appendChild(pic);
+                cardData.appendChild(h2);
+                cardData.appendChild(p1);
+                cardData.appendChild(p2);
+                cardData.appendChild(p3);
+                cardData.appendChild(p4);
+                
 
                 document.querySelector('div.towns').appendChild(card);
                 }
