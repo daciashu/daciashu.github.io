c4e699f391c60cdbb995f0bf794f37d74876ee9a
daciashu <cal98017@byui.edu>
2019-11-21 12:01:14 -0500

    api

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -8,7 +8,8 @@ fetch(apiURL)
 
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  
         const desc = jsObject.weather[0].description;  
+        const icon = document.createElement('img)');
         document.getElementById('imagesrc').textContent = imagesrc;  
         document.getElementById('icon').setAttribute('src', imagesrc);  
         document.getElementById('icon').setAttribute('alt', desc);
-    })
+    });
