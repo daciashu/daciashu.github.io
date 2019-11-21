759bddf6694122010ca43e0960841c7209fd0497
daciashu <cal98017@byui.edu>
2019-11-21 11:22:13 -0500

    s

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=b199c77eadf38caf121ee3f836208d11";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=b199c77eadf38caf121ee3f836208d11";
 
 fetch(apiURL)
     .then((response) => response.json())
