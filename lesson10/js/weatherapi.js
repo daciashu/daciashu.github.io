05bf6f1dd03bad3b5ec733d09233ef83a452c24b
daciashu <cal98017@byui.edu>
2019-11-21 11:15:28 -0500

    api test

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,7 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=b199c77eadf38caf121ee3f836208d11";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+    })
