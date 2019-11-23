c4cb3ef21500ecddd5bfe6fa0d9fa7804194a761
daciashu <cal98017@byui.edu>
2019-11-23 08:46:21 -0500

    °F

--- lesson10/js/weather.js
+++ lesson10/js/weather.js
@@ -22,7 +22,7 @@ fetch(apiURL)
         var f = (35.74 + (0.6215 * tempF) - (35.75 * s) + (0.4275 * (tempF * s))).toFixed(1);
     
         if( tempF <= 50 && speed > 3 ) {
-            return f;
+            return f + "°F";
         }
             else{
                 return "N/A";
