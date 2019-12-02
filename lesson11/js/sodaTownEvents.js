6f0a9b846433d282dae259168a6fe6b84f87d6a9
daciashu <cal98017@byui.edu>
2019-12-02 10:21:28 -0500

    events

---
+++ lesson11/js/sodaTownEvents.js
@@ -0,0 +1,34 @@
+const eventURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(eventURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject['towns'];
+        const townEvents = ["Soda Springs"]; 
+
+        for (let i=0; i < towns.length; i++) {
+            if (townEvents.includes(towns[i].name)) {
+            let card = document.createElement('article');
+            let cardData = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let p1 = document.createElement('p');
+            let pic = document.createElement('img');
+
+            cardData.className = 'town-events';
+
+            h2.textContent = 'Upcoming Events in' + towns[i].name;
+            p1.textContent = towns[i].events;
+            pic.setAttribute('src', "images/Fresh-Mineral-Water.jpg" + towns[i].photo);
+            pic.setAttribute('alt', "A picture of someone scooping a cup of water from the soda geyser");
+
+            card.appendChild(cardData);
+            card.appendChild(pic);
+            cardData.appendChild(h2);
+            cardData.appendChild(p1);
+
+            document.querySelector('div.towns').appendChild(card);
+        }
+      }
+    });
