240ad6db3ace88ea08c368435ed82d771b184425
daciashu <cal98017@byui.edu>
2019-12-02 11:06:17 -0500

    t

--- lesson11/js/sodaTownEvents.js
+++ lesson11/js/sodaTownEvents.js
@@ -14,16 +14,12 @@ fetch(eventURL)
             let cardData = document.createElement('div');
             let h2 = document.createElement('h2');
             let p1 = document.createElement('p');
-            let p2 = document.createElement('p');
-            let p3 = document.createElement('p');
             let pic = document.createElement('img');
 
             cardData.className = 'town-events';
 
             h2.textContent = 'Upcoming Events in ' + events[i].name;
             p1.textContent = events[i].events;
-            p2.textContent = events[i].events;
-            p3.textContent = events[i].events;
             pic.setAttribute('src', "images/Fresh-Mineral-Water.jpg");
             pic.setAttribute('alt', "A picture of someone scooping a cup of water from the soda geyser");
 
@@ -31,8 +27,6 @@ fetch(eventURL)
             card.appendChild(pic);
             cardData.appendChild(h2);
             cardData.appendChild(p1);
-            cardData.appendChild(p2);
-            cardData.appendChild(p3);
 
             document.querySelector('div.events').appendChild(card);
         }
