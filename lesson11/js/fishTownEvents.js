a8133f58682639a1797c9da64767f90e55c8e8bb
daciashu <cal98017@byui.edu>
2019-12-02 11:14:36 -0500

    events

--- lesson11/js/fishTownEvents.js
+++ lesson11/js/fishTownEvents.js
@@ -10,23 +10,32 @@ fetch(eventURL)
 
         for (let i=0; i < towns.length; i++) {
             if (townEvents.includes(towns[i].name)) {
-            let card = document.createElement('article');
-            let cardData = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let p1 = document.createElement('p');
-            let pic = document.createElement('img');
-
-            cardData.className = 'town-events';
-
-            h2.textContent = 'Upcoming Events in' + towns[i].name;
-            p1.textContent = towns[i].events;
-            pic.setAttribute('src', "images/lightTree.jpg" + towns[i].photo);
+                let card = document.createElement('div');
+                let cardData = document.createElement('div');
+                let h2 = document.createElement('h2');
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+                let p4 = document.createElement('p');
+                let pic = document.createElement('img');
+    
+                cardData.className = 'town-events';
+    
+                h2.textContent = 'Upcoming Events in ' + events[i].name;
+                p1.textContent = events[i].events[0];
+                p2.textContent = events[i].events[1];
+                p3.textContent = events[i].events[2];
+                p4.textContent = events[i].events[3];
+            pic.setAttribute('src', "images/lightTree.jpg");
             pic.setAttribute('alt', "The Town's Lit Christmas Tree at Night");
 
             card.appendChild(cardData);
             card.appendChild(pic);
             cardData.appendChild(h2);
             cardData.appendChild(p1);
+            cardData.appendChild(p2);
+            cardData.appendChild(p3);
+            cardData.appendChild(p4);
 
             document.querySelector('div.towns').appendChild(card);
         }
--- lesson11/js/prestonTownEvents.js
+++ lesson11/js/prestonTownEvents.js
@@ -10,23 +10,29 @@ fetch(eventURL)
 
         for (let i=0; i < towns.length; i++) {
             if (townEvents.includes(towns[i].name)) {
-            let card = document.createElement('article');
-            let cardData = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let p1 = document.createElement('p');
-            let pic = document.createElement('img');
-
-            cardData.className = 'town-events';
-
-            h2.textContent = 'Upcoming Events in' + towns[i].name;
-            p1.textContent = towns[i].events;
-            pic.setAttribute('src', "images/napoleon300.jpg" + towns[i].photo);
+                let card = document.createElement('div');
+                let cardData = document.createElement('div');
+                let h2 = document.createElement('h2');
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+                let pic = document.createElement('img');
+    
+                cardData.className = 'town-events';
+    
+                h2.textContent = 'Upcoming Events in ' + events[i].name;
+                p1.textContent = events[i].events[0];
+                p2.textContent = events[i].events[1];
+                p3.textContent = events[i].events[2];
+            pic.setAttribute('src', "images/napoleon300.jpg");
             pic.setAttribute('alt', "A picture of the cast of Napoleon Dynomite");
 
             card.appendChild(cardData);
             card.appendChild(pic);
             cardData.appendChild(h2);
             cardData.appendChild(p1);
+            cardData.appendChild(p2);
+            cardData.appendChild(p3);
 
             document.querySelector('div.towns').appendChild(card);
         }
